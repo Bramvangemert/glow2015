4159652f9f2913b9e315ebeaefefccbfc67d2a5c
Bram van Gemert <Bram1@bjs-MacBook-Pro.local>
2015-09-17 10:06:33 +0200

    Initial version

---
+++ glow2015/AboutViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AboutViewController.swift
+//  glow2015
+//
+//  Created by bj gemert on 17/09/15.
+//  Copyright (c) 2015 bj gemert. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet weak var TextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func show(sender: UIButton) {
+        var alertView =
+        UIAlertView(
+            title: "your text is:",
+            message: TextField.text,
+            delegate: nil,
+            cancelButtonTitle: "Done!"
+        );
+        alertView.show();
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
